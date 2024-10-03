a6a6adbc048f44267549eb7b12948cfbec70610b
luzzif <federico.luzzi@proton.me>
2024-10-03 11:56:23 +0100

    feat: add usdt on holesky

--- generate.ts
+++ generate.ts
@@ -50,6 +50,8 @@ const testnetIcons: TokenIcons = lowercaseAddressKeys({
             "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png",
         "0x7d98346b3b000c55904918e3d9e2fc3f94683b01":
             "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png",
+        "0x13c75ad40d4d30e5c057c3db52e74fdc0691f589":
+            "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png",
     },
     [SupportedTestnet.CeloAlfajores]: {
         "0xf194afdf50b03e69bd7d057c1aa9e10c9954e4c9":
