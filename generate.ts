94ee4af8b04cae5dcb346f379ce978648b4e3821
Paolo Guerra <pg.guerrap@proton.me>
2024-10-15 10:44:47 +0200

    feat: add token icons for taiko chain

--- generate.ts
+++ generate.ts
@@ -15,6 +15,7 @@ enum SupportedMainnet {
     Mode = SupportedChain.Mode,
     Mantle = SupportedChain.Mantle,
     Base = SupportedChain.Base,
+    Taiko = SupportedChain.Taiko,
 }
 
 type TokenIcons = Record<number, Record<Address, string>>;
@@ -56,7 +57,7 @@ const testnetIcons: TokenIcons = lowercaseAddressKeys({
     },
     [SupportedTestnet.CeloAlfajores]: {
         "0xf194afdf50b03e69bd7d057c1aa9e10c9954e4c9":
-            "https://assets.coingecko.com/coins/images/11090/standard/InjXBNx9_400x400.jpg?1696511031",
+            "https://assets.coingecko.com/coins/images/11090/standard/InjXBNx9_400x400.jpg",
         "0x10c892a6ec43a53e45d0b916b4b7d383b1b78c0f":
             "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/celo/assets/0xD8763CBa276a3738E6DE85b4b3bF5FDed6D6cA73/logo.png",
         "0x2043d9aa54e333c52db22a8afbfcbdce35958f42":
@@ -66,7 +67,7 @@ const testnetIcons: TokenIcons = lowercaseAddressKeys({
     },
     [SupportedTestnet.MantleSepolia]: {
         "0xb1eda18c1b730a973dac2ec37cfd5685d7de10dd":
-            "https://assets.coingecko.com/coins/images/30980/standard/token-logo.png?1696529819",
+            "https://assets.coingecko.com/coins/images/30980/standard/token-logo.png",
         "0xc8e265d4c037b0e0641c84b440ab260f4fdafd24":
             "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png",
         "0xd1d3cf05ef211c71056f0af1a7fd1df989e109c3":
@@ -81,13 +82,13 @@ const mainnetIcons: TokenIcons = lowercaseAddressKeys({
         "0x4200000000000000000000000000000000000006":
             "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png",
         "0x6863fb62ed27a9ddf458105b507c15b5d741d62e":
-            "https://assets.coingecko.com/coins/images/37504/standard/kim.jpg?1714579040",
+            "https://assets.coingecko.com/coins/images/37504/standard/kim.jpg",
         "0xdfc7c877a950e49d2610114102175a06c2e3167a":
-            "https://assets.coingecko.com/coins/images/34979/standard/MODE.jpg?1714561871",
+            "https://assets.coingecko.com/coins/images/34979/standard/MODE.jpg",
     },
     [SupportedMainnet.Mantle]: {
         "0x78c1b0c915c4faa5fffa6cabf0219da63d7f4cb8":
-            "https://assets.coingecko.com/coins/images/30980/standard/token-logo.png?1696529819",
+            "https://assets.coingecko.com/coins/images/30980/standard/token-logo.png",
         "0xdeaddeaddeaddeaddeaddeaddeaddeaddead1111":
             "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png",
         "0x09bc4e0d864854c6afb6eb9a9cdf58ac190d0df9":
@@ -97,11 +98,23 @@ const mainnetIcons: TokenIcons = lowercaseAddressKeys({
     },
     [SupportedMainnet.Base]: {
         "0x5dC25aA049837B696d1dc0F966aC8DF1491f819B":
-            "https://assets.coingecko.com/coins/images/37504/standard/kim.jpg?1714579040",
+            "https://assets.coingecko.com/coins/images/37504/standard/kim.jpg",
         "0x7B8C5d97c25B65f89817E8046851A32e963fc9cD":
-            "https://assets.coingecko.com/coins/images/37504/standard/kim.jpg?1714579040",
+            "https://assets.coingecko.com/coins/images/37504/standard/kim.jpg",
         "0x0555E30da8f98308EdB960aa94C0Db47230d2B9c":
-            "https://assets.coingecko.com/coins/images/7598/standard/wrapped_bitcoin_wbtc.png?1696507857",
+            "https://assets.coingecko.com/coins/images/7598/standard/wrapped_bitcoin_wbtc.png",
+    },
+    [SupportedMainnet.Taiko]: {
+        "0xA51894664A773981C6C112C43ce576f315d5b1B6":
+            "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png",
+        "0x07d83526730c7438048D55A4fc0b850e2aaB6f0b":
+            "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png",
+        "0xA9d23408b9bA935c230493c40C73824Df71A0975":
+            "https://assets.coingecko.com/coins/images/38058/standard/icon.png",
+        "0xc4C410459fbaF8f7F86b6cEE52b4fA1282FF9704":
+            "https://assets.coingecko.com/coins/images/7598/standard/wrapped_bitcoin_wbtc.png",
+        "0x7d02A3E0180451B17e5D7f29eF78d06F8117106C":
+            "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png",
     },
 });
 
